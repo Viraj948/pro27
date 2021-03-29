24b4c2ce16097f347b61d43e1a76719db5f40aa9
Viraj948 <74658897+Viraj948@users.noreply.github.com>
2021-03-29 13:33:35 +0530

    Add files via upload

--- bob.js
+++ bob.js
@@ -1,29 +1,29 @@
 class Bob {
-    constructor(x, y, r) {
-      var options = {
-          'isStatic':true,
-          'restitution':0.3,
-          'friction':0.5,
-          'density':1.2
-      }
-      this.body = Bodies.circle(x, y,r,options);
-     this.x=x;
-     this.y=y 
-     this.r=r 
-  
-     
-      World.add(world, this.body);
+  constructor(x, y, r) {
+    var options = {
+        'isStatic':true,
+        'restitution':0.3,
+        'friction':0.5,
+        'density':1.2
     }
-    display(){
-      var pos =this.body.position;
-      var bobDiameter=(this.r)*2
-      push();
+    this.body = Bodies.circle(x, y,r,options);
+   this.x=x;
+   this.y=y 
+   this.r=r 
 
-      translate(pos.x, pos.y);
-      rectMode(CENTER);
-      fill ("orange")
+   
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var bobDiameter=(this.r)*2
+    push();
 
-      ellipse( 0,0,this.r,this.r);
-      pop()
-    }
-  };
+    translate(pos.x, pos.y);
+    rectMode(CENTER);
+    fill ("orange")
+
+    ellipse( 0,0,this.r,this.r);
+    pop()
+  }
+};
